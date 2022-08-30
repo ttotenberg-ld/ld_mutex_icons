23e45f8f1062558ecc9aad4f15cdcea3ce9785a7
Tom Totenberg <>
2022-08-30 14:32:22 +0000

    Changed from re-rendering to checking for changes before re-rendering

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import ldclient
 from ldclient.config import Config
 import os
 from prettytable import PrettyTable as pt
-import sys
 import time
 import unicodedata
 from utils.create_user import random_ld_user
-from dotenv import load_dotenv
+
 
 '''
 Create a terminal and clear it
@@ -16,13 +15,14 @@ Create a terminal and clear it
 term = blessed.Terminal()
 print(term.clear)
 
+
 '''
 Set sdk_key and feature_flag_key to your LaunchDarkly keys, then initialize the LD client
 '''
-load_dotenv()
-sdk_key = os.environ.get('SDK_KEY')
-feature_flag_key = os.environ.get('FLAG_KEY')
+sdk_key = os.environ['SDK_KEY']
+feature_flag_key = os.environ['FLAG_KEY']
 ldclient.set_config(Config(sdk_key))
+
 
 '''
 Define symbols for the table
@@ -30,12 +30,13 @@ Define symbols for the table
 true_icon = unicodedata.lookup('WHITE HEAVY CHECK MARK')
 false_icon = unicodedata.lookup('CROSS MARK')
 
+
 '''
 Create fake targets for this exercise
 '''
 def create_targets():
-    num_targets = 15
-    users_array = {'users':[]}
+    num_targets = 40
+    users_array = {'users': []}
     for i in range(num_targets):
         # f = open('data/targets.json', 'w')
         user = random_ld_user()
@@ -43,20 +44,25 @@ def create_targets():
         with open('data/targets.json', 'w') as f:
             json.dump(users_array, f)
 
+
 '''
 Adds targets to the table
 '''
 def add_targets_to_table(data):
     target_table = pt()
     # print(term.clear)
-    target_table.field_names = ['key', 'firstName', 'lastName', 'version', 'plan', 'lang', '?']
+    target_table.field_names = [
+        'key', 'name', 'version', 'plan', 'region', 'Targeted?'
+    ]
     for i in data['users']:
-        feature = ldclient.get().variation("toms-beautiful-feature", i, False)
+        feature = ldclient.get().variation(feature_flag_key, i, False)
         if feature:
             feature = true_icon
         else:
             feature = false_icon
-        target_table.add_row([i['key'][0:5] + '...', i['firstName'], i['lastName'], i['custom']['version'], i['custom']['plan'], i['custom']['lang'], str(feature)])
+        target_table.add_row([
+            i['key'][0:8] + '...', i['name'], i['custom']['version'], i['custom']['plan'], i['custom']['region'], str(feature)
+        ])
     return target_table
 
 
@@ -64,19 +70,35 @@ def add_targets_to_table(data):
 Clears the terminal, then renders the table
 '''
 def render_table(table):
-    # with term.location(0, term.height - 1):
     with term.hidden_cursor():
         print(term.home + term.clear, end='')
         print(table)
 
 
 '''
-Persistent loop that keeps rerendering the table multiple times per second
+OK, word of warning: Everything below is pretty hacky. :)
+Ideally, I wanted to subscribe to flag changes and render the table only when there was an update. However, once I was mostly done creating this, I realized the Python SDK doesn't have that functionality yet! (as of fall 2022)
+
+https://docs.launchdarkly.com/sdk/features/flag-changes
+
+So instead, I'm saving an array of trues/falses for each target user, updating that array multiple times per second based on the latest targeting data from LaunchDarkly, and re-rendering the table if that has been updated.
+
+Don't judge me! It's hacky but it works!
 '''
 if __name__ == '__main__':
-    create_targets()
+    # Uncomment the line below and rerun the script if you want to generate new targets
+    # create_targets()
     data = json.load(open("data/targets.json"))
+    target_array = [False]
+    new_target_array = []
     while True:
-        table = add_targets_to_table(data)
-        render_table(table)
-        time.sleep(.01)
+        new_target_array = []
+        for i in data['users']:
+            feature = ldclient.get().variation(feature_flag_key, i, False)
+            new_target_array.append(feature)
+        if target_array != new_target_array:
+            table = add_targets_to_table(data)
+            render_table(table)
+            target_array = new_target_array
+            new_target_array = []
+        time.sleep(.1)
--- utils/create_user.py
+++ utils/create_user.py
@@ -8,20 +8,18 @@ Construct and return a random user
 '''
 def random_ld_user():
     key = str(uuid.uuid4())
-    first_name = names.get_first_name()
-    last_name = names.get_last_name()
+    name = f'{names.get_first_name()} {names.get_last_name()}'
     plan = random.choice(['platinum', 'silver', 'gold', 'diamond'])
-    version = random.choice(['1.0.2', '1.0.4,', '1.0.7', '1.1.0', '1.1.5'])
-    lang = random.choice(['EN', 'CN', 'SP', 'IN', 'GR'])
+    version = random.choice(['1.0.2', '1.0.4', '1.0.7', '1.1.0', '1.1.5'])
+    region = random.choice(['NA', 'CN', 'EU', 'IN', 'SA'])
 
     user = {
         "key": key,
-        "firstName": first_name,
-        "lastName": last_name,
+        "name": name,
         "custom": {
           "plan": plan,
           "version": version,
-          "lang": lang
+          "region": region
         }
     }
     return user
